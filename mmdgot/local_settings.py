132a6c9fe6e222d00ccd97ce9c2b416b3f78ef82
Dan Yang <dsyang92@gmail.com>
2012-06-06 22:36:32 -0700

    moved all code to mmdgot library

---
+++ mmdgot/local_settings.py
@@ -0,0 +1,26 @@
+'''
+Configuration Settings
+'''
+
+''' Uncomment to configure using the file.
+WARNING: Be careful not to post your account credentials on GitHub.
+
+TWILIO_ACCOUNT_SID = "ACxxxxxxxxxxxxx"
+TWILIO_AUTH_TOKEN = "yyyyyyyyyyyyyyyy"
+TWILIO_APP_SID = "APzzzzzzzzz"
+TWILIO_CALLER_ID = "+17778889999"
+'''
+'''
+
+This is the coffiguration for heroku's mongodb:
+MONGOLAB_URI = 'mongodb://heroku_app5033310:onqn6daqivk9' \
+'sekgbivnhp64hm@ds033757.mongolab.com:33757/heroku_app5033310'
+
+'''
+# Begin Heroku configuration - configured through environment variables.
+import os
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
+TWILIO_CALLER_ID = os.environ.get('TWILIO_CALLER_ID', None)
+TWILIO_APP_SID = os.environ.get('TWILIO_APP_SID', None)
+MONGOLAB_URI = os.environ.get('MONGOLAB_URI', None)
---
+++ mmdgot/views.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, request, redirect, render_template, url_for
+from flask.views import MethodView
+from mmdgot.models import Post, Comment
+from flaskext.mongoengine.wtf import model_form
+
+posts = Blueprint('posts', __name__, template_folder='templates')
+
+
+class ListView(MethodView):
+
+    def get(self):
+        posts = Post.objects.all()
+        return render_template('posts/list.html', posts=posts)
+
+
+class DetailView(MethodView):
+    form = model_form(Comment, exclude=['created_at'])
+
+    def get_context(self, slug):
+        post = Post.objects.get_or_404(slug=slug)
+        form = self.form(request.form)
+
+        context = {
+            "post": post,
+            "form": form
+        }
+        return context
+
+    def get(self, slug):
+        context = self.get_context(slug)
+        return render_template('posts/detail.html', **context)
+
+    def post(self, slug):
+        context = self.get_context(slug)
+        form = context.get('form')
+
+        if form.validate():
+            comment = Comment()
+            form.populate_obj(comment)
+
+            post = context.get('post')
+            post.comments.append(comment)
+            post.save()
+
+            return redirect(url_for('posts.detail', slug=slug))
+        return render_template('posts/detail.html', **context)
+
+posts.add_url_rule('/', view_func=ListView.as_view('list'))
+posts.add_url_rule('/<slug>/', view_func=DetailView.as_view('detail'))
